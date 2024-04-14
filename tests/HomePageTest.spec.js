06f9c1366c4cb480d7000210707abd1bcd34cac0
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-15 01:08:24 +0530

    created cusotm test to check url and title of repo page on github

--- tests/HomePageTest.spec.js
+++ tests/HomePageTest.spec.js
@@ -1,6 +1,20 @@
 const {test,expect} = require('@playwright/test');
 
-test('HomePage', ({page})=>{
+test('HomePage', async ({page})=>{
+    await page.goto('https://github.com/Sanidhya-Pundeer?tab=repositories');
+    
+    const pageTitle = await page.title();
+    console.log('Page title is: ', pageTitle);
 
+    const pageUrl = await page.url();
+    console.log('Page URL is: ', pageUrl);
+
+    //to apply the validation:
+    await expect(page).toHaveTitle('Sanidhya-Pundeer (Sanidhya-Pundeer) / Repositories · GitHub');
+    
+    await expect(page).toHaveURL('https://github.com/Sanidhya-Pundeer?tab=repositories');
+    await page.close();
+    
+    
 });
 
