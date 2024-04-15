72ea9a3589b8cf9d70a0cb93e211c4d25d63245a
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-15 23:51:55 +0530

    custom locator test in process

---
+++ tests/locators.spec.js
@@ -0,0 +1,14 @@
+// const {test, expect} = require('@playwright/test');
+//or
+import {test, expect} from '@playwright/test';
+
+test('locators', async ({page})=>{
+
+    await page.goto('https://www.demoblaze.com/');
+
+    
+
+
+
+});
+
