0950d09e49546fb5400d9b25d5b1eeb09e1f2891
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-23 09:41:41 +0530

    updated

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -6,7 +6,13 @@ test('locators', async ({page})=>{
 
     await page.goto('https://www.demoblaze.com/');
 
+    //locating element in playwright
+
+    //approach 1 - using property/id as a locator
+    await  page.locator('id=login2').click();
     
+    //approach 2
+    await page.click('id=login2');
 
 
 
